726592d9d65dd59ee1e558361d564c238e529cd5
git <390989068@qq.com>
2019-04-12 16:30:22 +0800

    添加moco

--- AutoTest/src/main/java/com/kenny/testng/extentReport/TestMethodDemo.java
+++ AutoTest/src/main/java/com/kenny/testng/extentReport/TestMethodDemo.java
@@ -1,22 +1,28 @@
 package com.kenny.testng.extentReport;
 
+import org.testng.Assert;
+import org.testng.Reporter;
 import org.testng.annotations.Test;
 
 public class TestMethodDemo {
     @Test
     public void test1(){
         System.out.println("这是test1执行！");
+        Assert.assertEquals(1,2);
     }
     @Test
     public void test2(){
         System.out.println("这是test1执行！");
+        Assert.assertEquals(1,1);
     }
     @Test
     public void test3(){
         System.out.println("这是test1执行！");
+        Assert.assertEquals("aaa","aaa");
     }
     @Test
     public void logDemo(){
-        System.out.println("记录日志！");
+        Reporter.log("这是我的自己的日志");
+        throw new RuntimeException("这是运行时异常");
     }
 }
